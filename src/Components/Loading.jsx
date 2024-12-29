d4613f9a4e3a4265bef0a241bd0b813e6e0d0f94
nadeemtadvi <nadeemtadvi1@gmail.com>
2024-12-29 20:40:30 +0530

    Add skeleton effect

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -9,10 +9,10 @@ const Loading = () => {
             key={index}
             className={`border-r border-b border-gray-200 md:w-[400px]`}
           >
-            <div className="p-[0.5rem_0.5rem_0] sm:p-[2rem_2rem_0] ">
+            <div className="m-[0.5rem_0.5rem_0] sm:m-[2rem_2rem_0] ">
               <div className="h-[160px] animate-pulse sm:h-[200px] bg-gray-300"></div>
             </div>
-            <div className="p-[0.5rem] sm:p-[0.7rem_2rem_0.7rem]">
+            <div className="m-[0.5rem] sm:m-[0.7rem_2rem_0.7rem]">
               <div>
                 <h5 className="mb-2 w-full h-[24px] bg-gray-300 animate-pulse"></h5>
               </div>
